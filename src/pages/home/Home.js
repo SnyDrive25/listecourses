3589454f2e8be86eed2e2ebcedf541d2f4b1c588
SnyDrive ~ Sunil <sunil.goulamhoussen@edu.devinci.fr>
2023-02-05 00:02:57 +0100

    local data saving and updating

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -6,68 +6,129 @@ function Home() {
 
   var [liste, setListe] = useState([]);
 
-  localStorage.setItem("liste", JSON.stringify(liste));
+  if (localStorage.getItem("liste") === undefined || localStorage.getItem("liste") === null) {
 
-  setInterval(function () {
-    // let div = document.getElementById("card");
-    // let spans = div.getElementsByTagName("span");
-    // let textArray = [];
-    // for (let i = 0; i < spans.length; i++) {
-    //   textArray.push(spans[i].textContent);
-    // }
-    //if (textArray.length >= JSON.parse(localStorage.getItem("liste")).length) {
-    if (JSON.parse(localStorage.getItem("liste")) !== undefined) {
-      setListe(JSON.parse(localStorage.getItem("liste")));
+    localStorage.setItem("liste", JSON.stringify([]));
+
+  }
+  else {
+
+    if (JSON.parse(localStorage.getItem("liste").length) === 0) {
+
+      localStorage.setItem("liste", JSON.stringify([]));
     }
-  }, 500);
+
+  }
+
+  setTimeout(() => {
+
+    let div = document.getElementById("card");
+    let ptags = div.getElementsByTagName("p");
+    let textArray = [];
+
+    if (ptags.length === 0 && localStorage.getItem("liste") !== undefined && localStorage.getItem("liste") !== null) {
+
+      if (JSON.parse(localStorage.getItem("liste")).length !== 0) {
+
+        const retrievedArray = JSON.parse(localStorage.getItem('liste'));
+
+        for (let i = 0; i < retrievedArray.length; i++) {
+          if (textArray.includes(retrievedArray[i]) === false) {
+            var code_element = retrievedArray[i][0].replaceAll(" ", "_").toLowerCase();
+            document.getElementById("card").innerHTML += "<p id='e" + code_element + "' onclick='e" + code_element + ".parentNode.removeChild(e" + code_element + ")'>" + retrievedArray[i][0] + "<span>" + retrievedArray[i][1] + "</span></p>";
+          }
+        }
+
+        localStorage.setItem("liste", JSON.stringify(retrievedArray));
+
+      }
+
+    }
+
+  }, 25);
+
+  setInterval(() => {
+
+    let div = document.getElementById("card");
+    let ptags = div.getElementsByTagName("p");
+    let textArray = [];
+
+    for (let i = 0; i < ptags.length; i++) {
+      textArray.push([ptags[i].firstChild.textContent, ptags[i].children[0].innerText]);
+    }
+
+    if (JSON.parse(localStorage.getItem("liste")) !== null && JSON.parse(localStorage.getItem("liste")) !== undefined) {
+
+      if (textArray.length < JSON.parse(localStorage.getItem("liste")).length) {
+
+        setListe(textArray);
+        localStorage.setItem("liste", JSON.stringify(textArray));
+
+      }
+      else {
+
+        setListe(textArray);
+        localStorage.setItem("liste", JSON.stringify(textArray));
+
+      }
+
+    }
+
+  }, 1000);
 
   function search() {
+
     var element = document.getElementById("new_element").value.replace(/[^a-zA-Z0-9\s]/g, "");
     // On enlève tout ce qui n'est pas un caractère alphabétique (a-zA-Z), numériques (0-9) ou un espace (\s) avec une expression régulière
 
-    if (element.length === 0) {
-      alert("Veuillez remplir la zone de texte avant d'ajouter un élément");
-    }
-    else {
-      element = element[0].charAt(0).toUpperCase() + element.slice(1).toLowerCase();
-      if (liste.includes(element)) {
-        alert("Cet élément est déjà dans la liste !");
-      }
-      else {
-        if (element !== "") {
-          var old_list = JSON.parse(localStorage.getItem("liste"));      // on récupère la liste actuelle
-          setListe([...old_list, element]);                              // on ajoute le nouvel élément dans le tableau
+    var quantite = parseInt(document.getElementById("quantite").value.replace(/[^0-9]/g, ""));
+    // On enlève tout ce qui n'est pas un caractère numériques (0-9) avec une expression régulière + on le convertit en entier
 
-          var code_element = element.replaceAll(" ", "_").toLowerCase();
-          document.getElementById("card").innerHTML += "<span id='e" + code_element + "' onclick='e" + code_element + ".parentNode.removeChild(e" + code_element + ")'>" + element + "</span>";
+
+    if (element.length === 0) {
+
+      alert("Veuillez remplir la zone de texte avant d'ajouter un élément");    // On empêche la liste de s'aggrémenter d'un élément vide et on avertit l'utilisateur
+
+    }
+
+    else {
+
+      element = element[0].charAt(0).toUpperCase() + element.slice(1).toLowerCase();    // Pour avoir l'élement écrit avec une première lettre en majuscule et les autres en minuscule
+
+      if (liste.includes(element)) {
+
+        alert("Cet élément est déjà dans la liste !");      // On avertit l'utilisateur si l'élément est déjà dans la liste
+
+      }
+
+      else {
+
+        if (element !== "") {
+
+          var old_list = JSON.parse(localStorage.getItem("liste"));      // on récupère la liste actuelle
+
+          setListe([...old_list, [element, quantite]]);                  // on ajoute le nouvel élément dans le tableau
+
+          var code_element = element.replaceAll(" ", "_").toLowerCase(); // on génère un id à partir du nom de l'élément
+
+          document.getElementById("card").innerHTML += "<p id='e" + code_element + "' onclick='e" + code_element + ".parentNode.removeChild(e" + code_element + ")'>" + element + "<span>" + quantite + "</span></p>";
           // Là on ajoute dans le html l'élément écrit et on lui donne une propriété onclick pour qu'il puisse être retiré en cliquant dessus
           // Remarque : j'ai ajouté un 'e' devant l'id et les références de l'id car sinon, si l'élément commence par un chiffre (ex : 2kg de riz), l'id ne sera pas valide car commençant par un chiffre !
 
           localStorage.setItem("liste", JSON.stringify(liste));          // on met à jour la variable du localstorage
-          setTimeout(() => {
-            document.getElementById("new_element").value = '';
-          }, 50);
-        }
-      }
-    }
-    document.getElementById("new_element").focus();
-  }
 
-  function sync() {
-    const retrievedArray = JSON.parse(localStorage.getItem('liste')) || [];
-    setListe(retrievedArray);
-    let div = document.getElementById("card");
-    let spans = div.getElementsByTagName("span");
-    let textArray = [];
-    for (let i = 0; i < spans.length; i++) {
-      textArray.push(spans[i].textContent);
-    }
-    for (let i = 0; i < retrievedArray.length; i++) {
-      if (textArray.includes(retrievedArray[i]) === false) {
-        var code_element = retrievedArray[i].replaceAll(" ", "_").toLowerCase();
-        document.getElementById("card").innerHTML += "<span id='e" + code_element + "' onclick='e" + code_element + ".parentNode.removeChild(e" + code_element + ")'>" + retrievedArray[i] + "</span>";
+          // on réinitialise nos deux zones de texte
+          document.getElementById("new_element").value = '';
+          document.getElementById("quantite").value = '';
+
+        }
+
       }
+
     }
+
+    document.getElementById("new_element").focus();
+
   }
 
   return (
@@ -76,6 +137,7 @@ function Home() {
       <button className='sync'>Synchroniser avec les données du navigateur</button>
       <div className='card'>
         <input type="text" placeholder='Oignons, Poulet, Jus, ...' id="new_element"></input>
+        <input type="number" placeholder='Ex : 4' id="quantite"></input>
         <button id="button" onClick={() => search()}>Ajouter</button>
       </div>
       <div className='card big' id="card"></div>

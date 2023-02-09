78f70ffae5eff80ae0447422e3c7b4fddb0d8c80
SnyDrive ~ Sunil <sunil.goulamhoussen@edu.devinci.fr>
2023-02-10 00:16:44 +0100

    about page lol

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -5,10 +5,16 @@ function About() {
 
   return (
     <div className="page">
+      <div className='courses'>
+        <p>
+
+        </p>
+      </div>
       <div className="social-btns">
         <a href="https://www.twitter.com/snydrive" target="_blank" rel='noreferrer'><i className="fa fa-twitter"></i></a>
         <a href="https://www.linkedin.com/in/sunil-goulamhoussen/" target="_blank" rel='noreferrer'><i className="fa fa-linkedin"></i></a>
         <a href="mailto:sunil.goulamhoussen@edu.devinci.fr" target="_blank" rel='noreferrer'><i className="fa fa-envelope"></i></a>
+        <a href="https://sunil.fr" target="_blank" rel='noreferrer'><i className="fa fa-globe"></i></a>
       </div>
     </div>
   );
